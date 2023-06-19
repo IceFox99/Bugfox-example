c6b49b85aa7532cf849b6bd534be6528860c45c2
IceFox99 <huyuefeng99@gmail.com>
2023-06-20 04:34:56 +0900

    Added a simple test.

--- src/math.js
+++ src/math.js
@@ -12,7 +12,7 @@ module.exports.sub = sub;
 const sum = (...args) => {
 	let total = 0;
 	for (let num of args)
-		total += num;
+		total = add(total, num); 
 	return total;
 };
 module.exports.sum = sum;
